8d08e398ddbef213e019d1ef646f92001a450653
Melanie <melanie.collins.p@gmail.com>
2024-03-15 12:47:15 -0300

    declarar elementos DOM y traer API con fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
 const btnPost = document.querySelector("#btn-post")
+const postData = document.querySelector("#post-data")
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
+
+// solicitud http: con fetch ($.ajax jquery)
+
+// fetch nos devuelve promesa, resolver con async y await o then
 
 const getData = async () => {
+    try {
+        const response = await fetch(API_URL)
 
-};
+        //validaciones error
+        if (!response.ok) {
+            console.log("la api falló")
+            console.log("código de error: ", response.status)
+        }
+        //procesar el json
+        const data = await response.json()
+        console.log(data)
 
-btnPost.addEventListener("click", getData)
+    } catch (error) {
+        console.log("error", error)
+    }
 
-// try {
-//     const res = await fetch("https://jsonplaceholder.typicode.com/posts" + id);
-//     const post = await res.json();
-//     console.log(post);
-// } catch (error) {
-//     console.log(error);
-// }
+}
+
+getData()
+
+// btnPost.addEventListener("click", getData)
