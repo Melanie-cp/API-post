1133ccf4fa5a144fd05473e4fb38db51d189cf00
Melanie <melanie.collins.p@gmail.com>
2024-03-15 12:14:58 -0300

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const btnPost = document.querySelector("#btn-post")
+
+const getData = async () => {
+
+};
+
+btnPost.addEventListener("click", getData)
+
+// try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts" + id);
+//     const post = await res.json();
+//     console.log(post);
+// } catch (error) {
+//     console.log(error);
+// }
