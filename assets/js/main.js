ac3b9d2079f92d6d9a0c56947ff64f09cd45857b
Melanie <melanie.collins.p@gmail.com>
2024-03-15 13:49:57 -0300

    se construye etiqueta li en HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,37 @@
+//declarar elementos del DOM
 const btnPost = document.querySelector("#btn-post")
 const postData = document.querySelector("#post-data")
 
+//constante API
 const API_URL = "https://jsonplaceholder.typicode.com/posts"
 
-// solicitud http: con fetch ($.ajax jquery)
+//no se usa el innerHTML ya que puede recibir código malicioso
+// const displayData = (data) => {
+//     data.forEach((element) => {
+//         postData.innerHTML += `<li><strong>${element.title}</strong><br>${element.body}</li>`;
+//     })
+// }
 
-// fetch nos devuelve promesa, resolver con async y await o then
-
+//construir etiqueta en HTML (document.createElement) para más seguridad
 const displayData = (data) => {
     data.forEach((element) => {
-        postData.innerHTML += `<li><strong>${element.title}</strong><br>${element.body}</li>`;
-    })
-}
+        const li = document.createElement("li");
+
+        const title = document.createElement("span");
+        title.textContent = element.title;
+        title.style.fontWeight = "bold";
+        li.appendChild(title);
+
+        const body = document.createElement("p");
+        body.textContent = element.body;
+        li.appendChild(body);
+
+        postData.appendChild(li);
+    });
+};
+
+// solicitud http: con fetch ($.ajax jquery)
+// fetch nos devuelve promesa, resolver con async y await o then
 
 const getData = async () => {
     try {
@@ -34,4 +54,5 @@ const getData = async () => {
 
 }
 
+//ejecutar al detectart click (getData sin paréntesis)
 btnPost.addEventListener("click", getData)
